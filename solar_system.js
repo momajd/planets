778df67534d81d4910f3253e01fb1f17e6571872
momajd <mo.a.majd@gmail.com>
2017-07-15 17:02:49 -0400

    zoom in and out

--- solar_system.js
+++ solar_system.js
@@ -1,4 +1,5 @@
-/* eslint no-undef:1, max-len:0*/
+/* eslint no-undef:1, max-len:0 */
+/* globals Planet Position Velocity */
 
 class SolarSystem {
   constructor (timeInterval) {
@@ -8,16 +9,24 @@ class SolarSystem {
   }
 
   addPlanets () {
-    let sun = new Planet("sun", 1.989e30, 695700, new Position(0, 0), new Velocity(0, 0), this);
-    let earth = new Planet("earth", 5.972e24, 6371, new Position(0, 1.496e8), new Velocity(30, 0), this);
-    let mars = new Planet("mars", 6.39e23, 3390, new Position(0, 2.279e8), new Velocity(24.1, 0), this);
-    let jupiter = new Planet("jupiter", 1.898e27, 69911, new Position(0, 7.785e8), new Velocity(13.1, 0), this);
-    let mercury = new Planet("mercury", 3.285e23, 2440, new Position(0, 5.791e7), new Velocity(47.4, 0), this);
-    let venus = new Planet("venus", 4.867e24, 6052, new Position(0, 1.082e8), new Velocity(35, 0), this);
-    let pluto = new Planet("pluto", 1.309e22, 1187, new Position(0, 4.44e9), new Velocity(4.75, 0), this);
-    let saturn = new Planet("saturn", 5.683e26, 58232, new Position(0, 1.429e9), new Velocity(9.67, 0), this);
-    let uranus = new Planet("uranus", 8.681e25, 25362, new Position(0, 2.871e9), new Velocity(6.84, 0), this);
-    let neptune = new Planet("neptune", 1.024e26, 24622, new Position(0, 4.498e9), new Velocity(5.48, 0), this);
+
+    let sunRadius = 695700; //in kilometers
+    sunRadius *= 50; //need to scale larger since radius is small compared to distance of planets
+    // scale other planets in terms of sun radius for visual purposes
+    // actual radii:
+    // earth: 6371, mars: 3390, jupiter: 69911, mercury 2440, venus: 6052,
+    // pluto: 1187, saturn 58232, uranus 25362, neptune 24622
+
+    let sun = new Planet("sun", 1.989e30, sunRadius, new Position(0, 0), new Velocity(0, 0), this);
+    let earth = new Planet("earth", 5.972e24, 1/4*sunRadius, new Position(0, 1.496e8), new Velocity(30, 0), this);
+    let mars = new Planet("mars", 6.39e23, 1/8*sunRadius, new Position(0, 2.279e8), new Velocity(24.1, 0), this);
+    let jupiter = new Planet("jupiter", 1.898e27, 3/4*sunRadius, new Position(0, 7.785e8), new Velocity(13.1, 0), this);
+    let mercury = new Planet("mercury", 3.285e23, 1/8*sunRadius, new Position(0, 5.791e7), new Velocity(47.4, 0), this);
+    let venus = new Planet("venus", 4.867e24, 1/4*sunRadius, new Position(0, 1.082e8), new Velocity(35, 0), this);
+    let pluto = new Planet("pluto", 1.309e22, 1/16*sunRadius, new Position(0, 4.44e9), new Velocity(4.75, 0), this);
+    let saturn = new Planet("saturn", 5.683e26, 3/4*sunRadius, new Position(0, 1.429e9), new Velocity(9.67, 0), this);
+    let uranus = new Planet("uranus", 8.681e25, 1/2*sunRadius, new Position(0, 2.871e9), new Velocity(6.84, 0), this);
+    let neptune = new Planet("neptune", 1.024e26, 1/2*sunRadius, new Position(0, 4.498e9), new Velocity(5.48, 0), this);
 
     let planets = [sun, earth, mars, jupiter, mercury, venus, pluto, saturn, uranus, neptune];
     planets.forEach(planet => this.planets.push(planet));
--- view.js
+++ view.js
@@ -5,9 +5,10 @@ class View {
     this.solarSystem = solarSystem;
 
     this.drawPlanets();
+    this.addZoom();
   }
 
-  drawPlanets() {
+  drawAllPlanets() {
     let ctx = this.context;
     let offsetX = ctx.canvas.width / 2; //use offsets to center the sun
     let offsetY = ctx.canvas.height / 2;
@@ -19,7 +20,7 @@ class View {
       ctx.arc(
         planet.position.x * scale + offsetX,
         planet.position.y * scale + offsetY,
-        planet.radius * 50 * scale, //scale planet radius larger
+        planet.radius * scale,
         0,
         2 * Math.PI
       );
@@ -28,10 +29,32 @@ class View {
       ctx.font="20px Georgia";
       ctx.fillText(
         planet.name,
-        (planet.position.x + 3/2 * planet.radius) * scale + offsetX,
+        (planet.position.x + 5/4 * planet.radius) * scale + offsetX,
         planet.position.y * scale + offsetY
       );
     });
+  }
+
+  drawPlanet() {
+    context.beginPath();
+    context.arc(cw/2,ch/2,75,0,Math.PI*2);
+    context.fillStyle='lightcyan';
+    context.fill();
+
+    context.globalCompositeOperation='source-atop';
+
+    context.shadowOffsetX = 500;
+    context.shadowOffsetY = 0;
+    context.shadowBlur = 15;
+    context.shadowColor = 'rgba(30,30,30,1)';
+
+    context.beginPath();
+    context.arc(cw/2-500,ch/2,75,0,Math.PI*2);
+    context.stroke();
+    context.stroke();
+    context.stroke();
+
+    context.globalCompositeOperation='source-over';
   }
 
   animate() {
@@ -40,5 +63,16 @@ class View {
       this.context.clearRect(0, 0, canvasEl.width, canvasEl.height);
       this.drawPlanets();
     }.bind(this), 10);
+  }
+
+  addZoom() {
+    this.context.canvas.addEventListener("mousewheel", function(e) {
+      if (this.scale > 0.00000001 && e.deltaY > 0) {
+        this.scale -= e.deltaY/1000000000;
+      } else if (this.scale < .00001 && e.deltaY < 0) {
+        this.scale -= e.deltaY/1000000000;
+      }
+      console.log(this.scale);
+    }.bind(this));
   }
 }
